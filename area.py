773a78a3ede9e539ff97bbb44c0c6f6c039358e2
rajesh <rajaga223@gmail.com>
2019-11-10 21:18:17 +0530

    initial commit

---
+++ area.py
@@ -0,0 +1,8 @@
+#print('hello')
+#str=input("hello what is name\n")
+#print (str +' hello ')
+#age = input('age ??\n')
+#print (str+" you're "+age)
+r  = input('enter the radius of circle(mts): \n')
+area=3.14*int(r)**2
+print ('area of circle',area)
---
+++ string.py
@@ -0,0 +1,4 @@
+num1 = 7524.321456
+num2 = 5.2364589
+#print('num  is {0:.5f} and num 2 is {1:.5f}'.format(num1,num2))
+print(f'num 1 is {num1:.4f} and num2 is {num2:.4f}')
