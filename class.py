c376730b04575d8274c85a73fb32072f5e35e787
rajesh <rajaga223@gmail.com>
2019-11-17 22:42:18 +0530

    class

---
+++ class.py
@@ -0,0 +1,22 @@
+class planet:
+
+    def __init__(self,name,gravity,radius,system):
+        self.name=name
+        self.garvity=gravity
+        self.radius=radius
+        self.system=system
+
+    def orbit(self):
+        return f'{self.name} is orbiting in the {self.system}'
+
+hoth=planet('hotho',9.8,6400,'solorsystem')
+print(f'name is {hoth.name}')
+print(f'gravit is {hoth.garvity}')
+print(hoth.orbit())
+
+
+planetx=planet('plan',5,5000,'plan')
+print(f'name of the planet is {planetx.name}')
+print(f'gravity of the planet {planetx.garvity}')
+print(f'radius of the planet is {planetx.radius}')
+print(planetx.orbit())
