c07e653ea4b2e6869ad8a60449ac619044ea8440
Lucifer___13 <rajaga223@gmail.com>
2020-03-05 21:43:53 +0530

    none

--- fileop.py
+++ fileop.py
@@ -7,3 +7,28 @@ else:
     f=open("file.txt","w+")
 
 f.open("file.txt","w+")
+ 
+# dir=input("Enter the project name :")
+# directory=dir
+
+# Parent_directory="c:/users/hp/documents/github/html"
+
+# path=os.path.join(Parent_directory,directory)
+
+
+# os.mkdir(path)
+# f=open(f"{path}index.html","w+")
+
+# style="style"
+# stypath=os.path.join(Parent_directory,directory,style)
+# if(os.path.exists(path)):
+#     os.mkdir(stypath)
+#     print("done")
+# else:
+#     print("undone")
+
+# def_path="c:/users/hp/documents/github/html"
+# os.chdir(def_path)
+
+# cmd=os.getcwd()
+# print(cmd)
