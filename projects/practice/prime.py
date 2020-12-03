a9196c39ff44b8289cda4f104963d4e77ec6f252
Rajesh jaga <Rajaga223@gmail.com>
2020-12-03 11:21:22 +0530

    last commit

--- projects/practice/prime.py
+++ projects/practice/prime.py
@@ -1,8 +1,7 @@
-i = int(input("push the intger"))
+i = input("push the intger")
 
-for x in range(1, i):
+# for x in range(1, i):
 
-    d = i % x
-    if d == 0:
-        print(x)
-
+#     d = i % x
+#     if d == 0:
+#         print(x)
---
+++ webscraping/main.py
@@ -0,0 +1,5 @@
+import wget
+
+
+url = 'http://www.futurecrew.com/skaven/song_files/mp3/razorback.mp3'
+filename = wget.download(url)
